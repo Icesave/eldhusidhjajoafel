19bf0599b9cf1d6d1c83cce62b4398fd9251031a
ofm1 <ofm1@hi.is>
2018-11-03 13:37:46 +0000

    initiation

---
+++ src/js/BUBBLETROUBLE.js
@@ -0,0 +1,130 @@
+// =========
+// BUBBLE TROUBLE
+// =========
+/*
+
+   TODO:  LÝSING Á LEIKNUM.
+
+
+*/
+
+"use strict";
+
+/* jshint browser: true, devel: true, globalstrict: true */
+
+var g_canvas = document.getElementById("myCanvas");
+var g_ctx = g_canvas.getContext("2d");
+
+/*
+0        1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+
+// ====================
+// CREATE INITIAL Player
+// ====================
+
+function createInitialPlayer() {
+
+   entityManager.generatePlayer({
+       cx : 200,
+       cy : 200
+   });
+   
+}
+
+
+// =================
+// UPDATE SIMULATION
+// =================
+
+
+// GAME-SPECIFIC UPDATE LOGIC
+
+function updateSimulation(du) {
+    
+    processDiagnostics();
+    
+    entityManager.update(du);
+
+    // Prevent perpetual firing!
+    //eatKey(Ship.prototype.KEY_FIRE);
+}
+
+// GAME-SPECIFIC DIAGNOSTICS
+
+var g_renderSpatialDebug = false;
+
+var KEY_SPATIAL = keyCode('X');
+
+var KEY_HALT  = keyCode('H');
+var KEY_RESET = keyCode('R');
+
+var KEY_K = keyCode('K');
+
+function processDiagnostics() {
+
+    if (eatKey(KEY_SPATIAL)) g_renderSpatialDebug = !g_renderSpatialDebug;
+
+    if (eatKey(KEY_RESET)) entityManager.resetPlayer();
+
+}
+
+
+// =================
+// RENDER SIMULATION
+// =================
+
+// We take a very layered approach here...
+//
+// The primary `render` routine handles generic stuff such as
+// the diagnostic toggles (including screen-clearing).
+//
+// It then delegates the game-specific logic to `gameRender`
+
+
+// GAME-SPECIFIC RENDERING
+
+function renderSimulation(ctx) {
+
+    entityManager.render(ctx);
+
+    if (g_renderSpatialDebug) spatialManager.render(ctx);
+}
+
+
+// =============
+// PRELOAD STUFF
+// =============
+
+var g_images = {};
+
+function requestPreloads() {
+
+    var requiredImages = {
+        // TODO: use another image for player
+        player   : "https://notendur.hi.is/~pk/308G/images/ship.png"
+    };
+
+    imagesPreload(requiredImages, g_images, preloadDone);
+}
+
+var g_sprites = {};
+
+function preloadDone() {
+
+    g_sprites.player  = new Sprite(g_images.player);
+    
+
+    g_sprites.bullet = new Sprite(g_images.player);
+    g_sprites.bullet.scale = 0.25;
+
+    entityManager.init();
+    createInitialPlayer();
+
+    main.init();
+}
+
+// Kick it off
+requestPreloads();
---
+++ src/js/entityManager.js
@@ -0,0 +1,135 @@
+/*
+
+entityManager.js
+
+A module which handles arbitrary entity-management for "Bubble trouble"
+
+
+We create this module as a single global object, and initialise it
+with suitable 'data' and 'methods'.
+
+"Private" properties are denoted by an underscore prefix convention.
+
+*/
+
+
+"use strict";
+
+
+// Tell jslint not to complain about my use of underscore prefixes (nomen),
+// my flattening of some indentation (white), or my use of incr/decr ops 
+// (plusplus).
+//
+/*jslint nomen: true, white: true, plusplus: true*/
+
+
+var entityManager = {
+
+// "PRIVATE" DATA
+
+_player   : [],
+_bullets : [],
+_balls : [],
+
+// "PRIVATE" METHODS
+
+_generateBall : function() {
+    
+    this.generateBall();
+},
+
+_forEachOf: function(aCategory, fn) {
+    for (var i = 0; i < aCategory.length; ++i) {
+        fn.call(aCategory[i]);
+    }
+},
+
+// PUBLIC METHODS
+
+// A special return value, used by other objects,
+// to request the blessed release of death!
+//
+KILL_ME_NOW : -1,
+
+// Some things must be deferred until after initial construction
+// i.e. thing which need `this` to be defined.
+//
+deferredSetup : function () {
+    this._categories = [this._player, this._bullets, this._balls];
+},
+
+init: function() {
+    this._generateBall();
+    
+},
+
+fireBullet: function(cx, cy, velX, velY, rotation) {
+    this._bullets.push(new Bullet({
+        cx   : cx,
+        cy   : cy
+    }));
+},
+
+generateBall : function(descr) {
+    this._balls.push(new Ball(descr));
+},
+
+generatePlayer : function(descr) {
+    this._player.push(new Player(descr));
+},
+
+
+resetPlayer: function() {
+    this._forEachOf(this._player, Player.prototype.reset);
+},
+
+
+update: function(du) {
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+        var i = 0;
+
+        while (i < aCategory.length) {
+
+            var status = aCategory[i].update(du);
+
+            if (status === this.KILL_ME_NOW) {
+                // remove the dead guy, and shuffle the others down to
+                // prevent a confusing gap from appearing in the array
+                aCategory.splice(i,1);
+            }
+            else {
+                ++i;
+            }
+        }
+    }
+    
+
+},
+
+render: function(ctx) {
+
+    var debugX = 10, debugY = 100;
+
+    for (var c = 0; c < this._categories.length; ++c) {
+
+        var aCategory = this._categories[c];
+
+
+        for (var i = 0; i < aCategory.length; ++i) {
+
+            aCategory[i].render(ctx);
+            //debug.text(".", debugX + i * 10, debugY);
+
+        }
+        debugY += 10;
+    }
+}
+
+}
+
+// Some deferred setup which needs the object to have been created first
+entityManager.deferredSetup();
+
