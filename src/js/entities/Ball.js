32efda9e0137553ecd26639b75a016e751b8f133
Icesave <elvararni@gmail.com>
2018-11-07 15:48:06 +0000

    Ball gravity - Elvar

--- src/js/entities/Ball.js
+++ src/js/entities/Ball.js
@@ -23,54 +23,37 @@ function Ball(descr) {
       
     // Default sprite and scale, if not otherwise specified
     this.sprite = this.sprite || g_sprites.ball;
-    this.scale  = this.scale  || 1;
+    this.scale  = this.scale  || 0.5;
 
 };
 Ball.prototype = new Entity();
 
-Ball.prototype.randomisePosition = function () {
-    // Ball randomisation defaults (if nothing otherwise specified)
-    this.cx = this.cx || Math.random() * g_canvas.width;
-    this.cy = this.cy || Math.random() * g_canvas.height;
-    this.rotation = this.rotation || 0;
-};
-
-Ball.prototype.randomiseVelocity = function () {
-    var MIN_SPEED = 20,
-        MAX_SPEED = 70;
-
-    var speed = util.randRange(MIN_SPEED, MAX_SPEED) / SECS_TO_NOMINALS;
-    var dirn = Math.random() * consts.FULL_CIRCLE;
-
-    this.velX = this.velX || speed * Math.cos(dirn);
-    this.velY = this.velY || speed * Math.sin(dirn);
-
-    var MIN_ROT_SPEED = 0.5,
-        MAX_ROT_SPEED = 2.5;
-
-    this.velRot = this.velRot ||
-        util.randRange(MIN_ROT_SPEED, MAX_ROT_SPEED) / SECS_TO_NOMINALS;
-};
-
+var NOMINAL_GRAVITY = 0.12;
 Ball.prototype.update = function (du) {
-      // Remember my previous position
-      var prevX = this.cx;
-      var prevY = this.cy;
+    // Remember my previous position
+    var prevX = this.cx;
+    var prevY = this.cy;
+
+    this.yVel += NOMINAL_GRAVITY;
   
-      // Compute my provisional new position (barring collisions)
-      this.nextX = prevX + this.xVel * du;
-      this.nextY = prevY + this.yVel * du;
+    // Compute my provisional new position (barring collisions)
+    this.nextX = prevX + this.xVel * du;
+    this.nextY = prevY + this.yVel * du;
 
     if (this._isDeadNow) {
         return entityManager.KILL_ME_NOW;
     }
-        // Bounce off top, bottom and side edges
-      if (this.nextY < 0 || this.nextX > g_canvas.height) {
-          this.yVel *= -1;
-      }
-      if (this.nextX < 0 || this.nextX > g_canvas.width) {
-          this.xVel *= -1;
-      }
+
+
+
+    var r = this.getRadius();
+    // Bounce off top, bottom and side edges
+    if (this.nextY < r || this.nextY > g_canvas.height - r) {
+        this.yVel *= -1;
+    }
+    if (this.nextX < r || this.nextX > g_canvas.width - r) {
+        this.xVel *= -1;
+    }
     
     this.cx += this.xVel * du;
     this.cy += this.yVel * du;
@@ -98,7 +81,7 @@ Ball.prototype._spawnFragment = function () {
     entityManager.generateBall({
         cx : this.cx,
         cy : this.cy,
-        scale : this.scale /2
+        scale : this.scale/2
     });
 };
 
@@ -106,7 +89,7 @@ Ball.prototype.render = function (ctx) {
     var origScale = this.sprite.scale;
     // pass my scale into the sprite, for drawing
     this.sprite.scale = this.scale;
-    this.sprite.drawWrappedCentredAt(
+    this.sprite.drawCentredAt(
         ctx, this.cx, this.cy, this.rotation
     );
 };
--- src/js/entityManager.js
+++ src/js/entityManager.js
@@ -35,7 +35,7 @@ _balls : [],
 
 _generateBall : function() {
     
-    this.generateBall({cx: 200, cy: 200, xVel: 3, yVel: 1});
+    this.generateBall({cx: 200, cy: 200, xVel: 2, yVel: 0});
 },
 
 _forEachOf: function(aCategory, fn) {
