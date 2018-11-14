394877518520d61d33235ccae548080cb6c0a16d
arnahlin <ahs25@hi.is>
2018-11-14 15:19:14 +0000

    changes in levels.js

--- src/js/levels.js
+++ src/js/levels.js
@@ -13,50 +13,43 @@ that the createLevel function uses to generate a level.
 
 // TO DO: implement some kind of method to choose a level, where to put it?
 
-var index = 0;  // This should be used to change levels 
-var levelIndex = Levels[index]; 
+
+//==============================
+function levelUp() {
+    index++;
+    createLevel(levelIndex);
+}
+
+function levelDown() {
+    index--;
+    createLevel(levelIndex);
+}
+//===============================
 
 var Levels = [
     {
         title: "Level 1",
-        backgroundColor: "#00ccff", // blue
-        ballNum: 1,
-        ballCX: 600,
-        ballCY: 200,
-        ballXvel: 2,
-        ballYvel: -10,
-        bricks: false
+        backgroundColor: "#00ccff", // blue (dæmi)
+        balls: [[{cx:600,cy:200,xVel:2,yVel:-10}]], //???? eða án cx... 
+        bricksOn: false
     }, 
     {
         title: "Level 2",
         backgroundColor: "#99ff66", // green
-        ballNum: 2,
-        ball1CX: 600,
-        ball1CY: 200,
-        ball2CX: 250,
-        ball2CY: 200,
-        ballXvel: 2,
-        ballYvel: -10,
-        bricks: false
+        balls: [[200,200,2,-10],[600,200,2,-10]],
+        bricksOn: false
     },
     {
         title: "Level 3",
         backgroundColor: "#ff6666", // pink
-        ballNum: 4,
-        ball1CX: 600,
-        ball1CY: 100,
-        ball2CX: 250,
-        ball2CY: 200,
-        ballXvel: 2,
-        ballYvel: -10,
-        bricks: true,
-        brick1CX: 900,  // create a box in the right
-        brick1CY: 200,  // corner to trap the balls
-        brick2CX: 800,
-        brick2CY: 100
+        balls: [[250,200,2,-10],[600,200,2,-10],[300,200,2,-10],[500,200,2,-10]],       
+        bricksOn: true,
+        bricks: [[900, 200],[800,100]], // þarf ekki cx, cy??                                       
     }
 ];
 
+var index = 0;  // This should be used to change levels 
+var levelIndex = Levels[index]; 
 
 // Maybe we want to put this into the entityManager instead? 
 
@@ -65,7 +58,14 @@ Creates levels with the information from the levels array
 */
 
 function createLevel(levelIndex) { 
+
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     
+    entityManager.generatePlayer({cx:200, cy:520});
+
+    // ====================== ATH
+    // Nota foreach lykkju á ball array til að búa til bolta. Sama með bricksOn
+/*
     if (levelIndex.ballNum = 1) { 
         entityManager.generateBall(levelIndex.ballCX,levelIndex.ballCY,levelIndex.ballXvel,levelIndex.ballYvel);
     }
@@ -81,8 +81,19 @@ function createLevel(levelIndex) {
             entityManager.generateBall(levelIndex.ball1CX-50,levelIndex.ball1CY-50,levelIndex.ballXvel,levelIndex.ballYvel);
         }
     }
+    */
 
-    if (levelIndex.bricks === true) {
+    levelIndex.balls.forEach(function(element) {
+        entityManager.generateBall(element);      
+    });
+
+
+    if (levelIndex.bricksOn === true) {
+        levelIndex.bricks.forEach(function(element) {
+            entityManager.generateBrick(element);      
+        });
+
+        /*
         entityManager.generateBrick({
                 cx: levelIndex.brick1CX,
                 cy: levelIndex.brick1CY,
@@ -97,4 +108,6 @@ function createLevel(levelIndex) {
                 strokeStyle : "#790000",
                 fillStyle : "#D20000"
         }); 
+        */
+    }
 }
